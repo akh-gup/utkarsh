11aa9e242c1905bfd58123a038ffb0f9e0425079
Akhil Gupta <akhil.gupta@etraveligroup.com>
2024-07-15 15:23:14 +0200

    First Commit

---
+++ src/main/java/com/personal/finance/utkarsh/StockInvestmentApplication.java
@@ -0,0 +1,13 @@
+package com.personal.finance.utkarsh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockInvestmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockInvestmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/finance/utkarsh/StockInvestmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.finance.utkarsh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockInvestmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
